c3a30a57c7da15095b9ac92401033a748f6ecdf2
romariogajog <romariomarcal11@gmail.com>
2024-02-28 00:50:50 +0900

    Coding Challenge Day 25

---
+++ 25day.go
@@ -0,0 +1,17 @@
+// 25/366
+
+// https://www.codewars.com/kata/523b4ff7adca849afe000035
+
+// Function 1 - Hello World
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Print(greet())
+}
+
+func greet() string {
+	return "hello world"
+}
