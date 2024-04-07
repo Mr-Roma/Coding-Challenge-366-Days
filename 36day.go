0666111038054140bd42764fc459d7da7405861d
Romario Marcal <romariomarcal11@gmail.com>
2024-04-08 00:47:07 +0900

    Coding Challenge Days 36

---
+++ 36day.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(CountPositivesSumNegatives([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15}))
+}
+
+func CountPositivesSumNegatives(numbers []int) []int {
+	var save []int
+	var positive int
+	var sumNegative int
+
+	positive = 0
+	sumNegative = 0
+
+	for i := 0; i < len(numbers); i++ {
+		if numbers[i] >= 0 {
+			positive++
+		} else {
+			sumNegative += numbers[i]
+		}
+	}
+	save = append(save, positive)
+	save = append(save, sumNegative)
+	return save
+}
